03e1bf599b37c18aadf89f7bf3fe051c685fe30e
Eric Negreido <ericnegreidooo@gmail.com>
2024-07-02 20:37:00 -0300

    pagina de login y 404

--- app/views/content/404-view.php
+++ app/views/content/404-view.php
@@ -1,3 +1,6 @@
-<?php
-
-echo "404";
+<div class="alert alert-light" role="alert">
+  <h4 class="alert-heading">Error 404</h4>
+  <p>Pagnina no encontrada</p>
+  <hr>
+  <p class="mb-0">Hubo un error al encontrar el recurso solicitado</p>
+</div>
