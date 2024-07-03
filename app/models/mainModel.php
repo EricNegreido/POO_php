9d8872a16ac9f4fec114b15b79103b55b487fad0
Eric Negreido <ericnegreidooo@gmail.com>
2024-07-03 20:33:19 -0300

    modelo principal, conexion a la base de datos, consulta preparada, filtro para evitar inyeccion

---
+++ app/models/mainModel.php
@@ -0,0 +1,39 @@
+<?php 
+  namespace app\models;
+  use \PDO;
+
+  if(file_exists(__DIR__."/../../config/server.php")){
+    require_once __DIR__."/../../config/server.php";
+  }
+  class mainModel{
+    private $server = $DB_SERVER;
+    private $db= $DB_NAME;
+    private $user= $DB_USER;
+    private $pass = $DB_PASS;
+
+    protected function conectar(){
+      $conexion = new PDO("mysql:host=". $this->server.";dbname=".$this->db, $this->user, $this->pass);
+      $conexion->exec("SET CHARACTER SET utf8");
+      return $conexion;
+    }
+
+    protected function ejecConsulta($consulta){
+      $sql = $this->conectar()->prepare($consulta);
+      $sql->execute();
+      return $sql;
+    }
+
+    public function limpiarCadena($cadena){
+      $palabras=["<script>","</script>","<script src","<script type=","SELECT * FROM","SELECT "," SELECT ","DELETE FROM","INSERT INTO","DROP TABLE","DROP DATABASE","TRUNCATE TABLE","SHOW TABLES","SHOW DATABASES","<?php","?>","--","^","<",">","==","=",";","::"];
+      $cadena = trim($cadena); //quitamos espacios vacios
+      $cadena = stripslashes($cadena);//quitamos barras invertidas
+      foreach($palabras as $palabra){
+        $cadena = str_ireplace($palabra, "", $cadena);// busca y reemplaza la palabra
+      }
+      $cadena = trim($cadena); //quitamos espacios vacios
+      $cadena = stripslashes($cadena);//quitamos barras 
+
+      return $cadena;
+
+    }
+  }
