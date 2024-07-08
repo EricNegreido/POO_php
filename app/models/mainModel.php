19aa0ad38cc5a47ba4202e921cfa8ed4442d55fa
Eric Negreido <ericnegreidooo@gmail.com>
2024-07-08 20:23:10 -0300

    funcion para eliminar registro

--- app/models/mainModel.php
+++ app/models/mainModel.php
@@ -38,16 +38,72 @@
     }
 
     protected function VerificarDatos($filtro, $cadena){
-      if(preg_match("/^".$filtro."$/", $cadena)){
-        return false;
-      }else{
-        return true;
-      }
+        if(preg_match("/^".$filtro."$/", $cadena)){
+          return false;
+        }else{
+          return true;
+        }
 
     }
 
     protected function guardarDatos($tabla, $datos){
-      $query="INSERT INTO $tabla (";
+        $query="INSERT INTO $tabla (";
+
+        $Count = 0;
+
+        foreach($datos as $clave){
+          if($Count > 0){ 
+            $query.= ","; 
+          }
+          $query.=$clave["campo_nombre"];
+          $Count++;
+    
+        }
+        $query.=") VALUES(";
+
+        $Count = 0;
+
+        foreach($datos as $clave){
+          if($Count > 0){ 
+            $query.= ","; 
+          }
+          $query.=$clave["campo_marcador"];
+          $Count++;
+    
+        }
+        $query.=")";
+        $sql=$this->conectar()->prepare($query);
+        foreach($datos as $clave){
+          $sql->bindParam($clave["campo_marcador"], $clave["campo_valor"]);
+
+
+      }
+      $sql->execute();
+      return $sql;
+      
+    }
+
+    public function seleccionaDatos($tipo, $tabla, $campo, $id){
+      $tipo = $this->limpiarCadena($tipo);
+      $tabla = $this->limpiarCadena($tabla);
+      $campo = $this->limpiarCadena($campo);
+      $id = $this->limpiarCadena($id);
+
+      if($tipo == "Unico"){
+        $sql = $this->conectar()->prepare("SELECT * FROM $tabla WHERE $campo=:ID");
+        $sql->bindParam(":ID", $id);
+      }elseif($tipo == "Normal"){
+        $sql = $this->conectar()->prepare("SELECT $campo FROM $tabla");
+      }
+
+      $sql->execute();
+
+      return $sql;
+
+    }
+    
+    protected function actualizarDatos($tabla, $datos, $condicion){
+      $query="UPDATE $tabla SET  (";
 
       $Count = 0;
 
@@ -55,75 +111,28 @@
         if($Count > 0){ 
           $query.= ","; 
         }
-        $query.=$clave["campo_nombre"];
+        $query.=$clave["campo_nombre"]."=".$clave["campo_marcador"];
         $Count++;
-  
-      }
-      $query.=") VALUES(";
 
-      $Count = 0;
-
-      foreach($datos as $clave){
-        if($Count > 0){ 
-          $query.= ","; 
-        }
-        $query.=$clave["campo_marcador"];
-        $Count++;
-  
       }
-      $query.=")";
+      $query.="WHERE".$condicion["condicion_campo"]."=".$condicion["condicion_campo"];
+
       $sql=$this->conectar()->prepare($query);
       foreach($datos as $clave){
         $sql->bindParam($clave["campo_marcador"], $clave["campo_valor"]);
-
-
     }
+    $sql->bindParam($condicion["condicion_marcador"], $condicion["campo_valor"]);
+    
     $sql->execute();
     return $sql;
     
-  }
-
-  public function seleccionaDatos($tipo, $tabla, $campo, $id){
-    $tipo = $this->limpiarCadena($tipo);
-    $tabla = $this->limpiarCadena($tabla);
-    $campo = $this->limpiarCadena($campo);
-    $id = $this->limpiarCadena($id);
-
-    if($tipo == "Unico"){
-      $sql = $this->conectar()->prepare("SELECT * FROM $tabla WHERE $campo=:ID");
-      $sql->bindParam(":ID", $id);
-    }elseif($tipo == "Normal"){
-      $sql = $this->conectar()->prepare("SELECT $campo FROM $tabla");
     }
 
-    $sql->execute();
-
-    return $sql;
-
-  }
-  protected function actualizarDatos($tabla, $datos, $condicion){
-    $query="UPDATE $tabla SET  (";
-
-    $Count = 0;
-
-    foreach($datos as $clave){
-      if($Count > 0){ 
-        $query.= ","; 
-      }
-      $query.=$clave["campo_nombre"]."=".$clave["campo_marcador"];
-      $Count++;
+    protected function eliminarRegistro($tabla, $campo, $id){
+      $sql=$this->conectar()->prepare("DELETE FROM $tabla WHERE $campo=:id");
+      $sql->bindParam(":id", $id);
+      $sql->execute();
+      return $sql;
 
     }
-    $query.="WHERE".$condicion["condicion_campo"]."=".$condicion["condicion_campo"];
-
-    $sql=$this->conectar()->prepare($query);
-    foreach($datos as $clave){
-      $sql->bindParam($clave["campo_marcador"], $clave["campo_valor"]);
-  }
-  $sql->bindParam($condicion["condicion_marcador"], $condicion["campo_valor"]);
-  
-  $sql->execute();
-  return $sql;
-  
-}
 }
